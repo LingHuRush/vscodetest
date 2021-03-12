3fab003e8fa8d5b1b5a55d40741993644d95ad0a
LingHuRush <1084048221@qq.com>
2021-03-12 09:54:07 +0800

    123

---
+++ iters.py
@@ -0,0 +1,16 @@
+class Squares:
+    def __init__(self,start,stop) -> None:
+        self.value=start-1
+        self.stop=stop
+        print(self)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.value==self.stop:
+            return StopIteration
+        self.value+=1
+        return self.value**2
+
+if __name__=='__main__':
+    for i in Squares(1,5):
+        print(i,end=' ')
