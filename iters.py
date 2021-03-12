cdeae292cfe9d72f0403b57259bea8afd2e543b3
LingHuRush <48249277+LingHuRush@users.noreply.github.com>
2021-03-12 10:05:37 +0800

    Update iters.py

--- iters.py
+++ iters.py
@@ -7,7 +7,7 @@ class Squares:
         return self
     def __next__(self):
         if self.value==self.stop:
-            return StopIteration
+            rasie StopIteration
         self.value+=1
         return self.value**2
 
